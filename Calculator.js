f4d46bb36820cceba50e1d3e506bdf586a2868be
henry mutua <mutuahenry116@gmail.com>
2023-05-21 21:26:13 +0300

    final version

--- Calculator.js
+++ Calculator.js
@@ -1,50 +1,85 @@
+let operator = '';
+let previousValue = '';
+let currentValue = '';
 
-const add = (...args) =>{
-let total = 0;
-for(const arg of args){
-    total +=arg;
-}
-return total;
+document.addEventListener("DOMContentLoaded", function(){
+    // Store all components on HTML in our JS
+    let clear = document.querySelector("#clear-btn");
+    let equal = document.querySelector(".equal");
+   
+    
+    let numbers = document.querySelectorAll(".numbers");
+    let operators = document.querySelectorAll(".operators");
 
-}
+    let previousScreen = document.querySelector(".previous");
+    let currentScreen = document.querySelector(".current");
 
-const subtract = (a,b, ...args) => {
-    let difference = 0;
-    for(const arg of args){
-        difference-=arg;
-    }
-    return difference;
-}
+    numbers.forEach((number) => number.addEventListener("click", function(e){
+        handleNumber(e.target.textContent)
+        currentScreen.textContent = currentValue;
+    }))
 
-const multiply = (a, b, ...args) =>{
-    let product = a*b;
-    for (const arg of args){
-        product*=arg;
-    }
-    return product;
-}
+    operators.forEach((op) => op.addEventListener("click", function(e){
+        handleOperator(e.target.textContent)
+        previousScreen.textContent = previousValue + " " + operator;
+        currentScreen.textContent = currentValue;
+    }))
 
-const divide = (a,b, ...args) => {
-    let divisionresult = a/b;
-    for (const arg of args){
-        divisionresult/=arg;
-    }
-    return divisionresult;
-}
+    clear.addEventListener("click", function(){
+        previousValue = '';
+        currentValue = '';
+        operator = '';
+        previousScreen.textContent = currentValue;
+        currentScreen.textContent = currentValue;
+    })
 
-let displayValue = 0;
-let displayArea = document.getElementByClassName('screen');
-displayArea.innerHTML += displayValue;
+    equal.addEventListener("click", function(){
+        if(currentValue != '' && previousValue != ''){
+            calculate()
+            previousScreen.textContent = '';
+            if(previousValue.length <= 5){
+                currentScreen.textContent = previousValue;
+            } else{
+                currentScreen.textContent = previousValue.slice(0,5) + "...";
+            }
+        }
+    })
 
-const button = document.querySelectorAll('numbers');
-button.forEach(numbers => {
-    addEventListener('click', (e)=>{
-    updateDisplay(e);
+    
 })
-})
 
-let updateDisplay = (e) => {
-    displayValue = e.target.id;
-    displayArea.innerHTML = displayValue;
+function handleNumber(num){
+    if(currentValue.length <= 5){
+        currentValue += num; 
+    }
+}
+
+function handleOperator(op){
+    operator = op;
+    previousValue = currentValue;
+    currentValue = '';
+}
+
+function calculate(){
+    previousValue = Number(previousValue);
+    currentValue = Number(currentValue);
+
+    if(operator === "+"){
+        previousValue += currentValue;
+    } else if(operator === "-"){
+        previousValue -= currentValue;
+    } else if(operator === "x"){
+        previousValue *= currentValue;
+    } else{
+        previousValue /= currentValue;
+    }
+
+    previousValue = roundNumber(previousValue);
+    previousValue = previousValue.toString();
+    currentValue = previousValue.toString();
+}
+
+function roundNumber(num){
+    return Math.round(num * 1000) / 1000;
 }
 
